e1bd8303720e322a225496db20c17ab21f686df1
Parsa Sadeghi Asl <parsasadeghiasl@gmail.com>
2024-08-19 22:54:30 +0330

    project settings adjustments

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media_root/'
+STATIC_ROOT = BASE_DIR / 'static_root/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
