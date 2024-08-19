c3c41c509b4a930d634d3b5b25b067880d811449
Parsa Sadeghi Asl <parsasadeghiasl@gmail.com>
2024-08-19 23:15:51 +0330

    added views

--- portfolio/webapp/views.py
+++ portfolio/webapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Home, About, Profile, Category, skills, portfolio
 
-# Create your views here.
+
+def index(request):
+    
+    home = Home.objects.latest('updated')
+    
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about= about)
+
+    
+    categories = Category.objects.all()
+    
+    portfolios = portfolio.objects.all()
+    
+    context = {
+        'home': home, 
+        'about': about, 
+        'profiles': profiles, 
+        'categories': categories, 
+        'portfolios': portfolios
+    }
+    
+    
+    return render(request,'index.html',context)
